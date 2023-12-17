81b36bfef29ea93ba125c26ace0f50bbb81bb84a
Thonatosss <sven4369@gmail.com>
2023-12-17 22:43:53 +0000

    Deploying to gh-pages from @ Thonatosss/dental@f7dc01dff991a063fe65bff273164b6a278dc401 🚀

--- index.bee89f78.js
+++
@@ -1,2 +0,0 @@
-window.scrollTo(0,1e3),window.scrollTo({top:1e3,behavior:"smooth"});
-//# sourceMappingURL=index.bee89f78.js.map
--- index.da3f897d.js
+++
@@ -1,2 +0,0 @@
-window.scrollTo(0,1e3),window.scrollTo({top:1e3,behavior:"smooth"});
-//# sourceMappingURL=index.da3f897d.js.map
